1c5e81c4993a4fb9020594319d87c1247a59e71d
GURUTECH <saingsaroeuninfo@gmail.com>
2024-08-12 18:52:26 +0700

    Second Commit

---
+++ Index.php
@@ -0,0 +1,19 @@
+
+<!-- 
+ …or create a new repository on the command line
+echo "# aga" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/saroeunsainginfo/aga.git
+git push -u origin main 
+
+-->
+
+
+<?php
+
+    echo "Hello World";
+
+?>
