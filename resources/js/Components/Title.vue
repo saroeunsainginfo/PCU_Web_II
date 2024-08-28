fb22657c3424fb3871f9c9ef44455c2bcb4170fc
rapanhalucky <rapanhalucky@gmail.com>
2024-08-28 18:10:10 +0700

    Updated

---
+++ resources/js/Components/Title.vue
@@ -0,0 +1,13 @@
+<script setup>
+const props = defineProps({
+    title: String,
+});
+</script>
+
+<template>
+    <div>
+        {{ title }}
+    </div>
+</template>
+
+<style></style>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,11 @@
 <script setup>
 import App from "../Layout/App.vue";
+import Title from "../Components/Title.vue";
 </script>
 
 <template>
     <App>
+        <Title title="Hello" />
         <h4 class="text-red-500">Hello, Developer</h4>
     </App>
 </template>
