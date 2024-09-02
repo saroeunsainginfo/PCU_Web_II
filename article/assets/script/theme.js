c1a691d1a0b43b204fbb143f2f067b17edc1870d
GURU SAROEUN <85205366+saing-saroeun@users.noreply.github.com>
2024-09-02 18:40:07 +0700

    Delete article directory

--- article/assets/script/theme.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.main-slide-banner').slick({
-        speed: 2000,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
-});
