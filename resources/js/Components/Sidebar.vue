c33050e1ff604091c9ec9adf86e9e372720fdfb3
rapanhalucky <rapanhalucky@gmail.com>
2024-09-08 23:35:14 +0700

    Updated Sidebar

--- resources/js/Components/Sidebar.vue
+++ resources/js/Components/Sidebar.vue
@@ -1,9 +1,173 @@
-<script setup></script>
+<script setup>
+import NotificationIcon from './Icons/NotificationIcon.vue';
+import MenuItem from './MenuItem.vue';
+import ReportIcon from './Icons/ReportIcon.vue';
+import RiskIcon from './Icons/RiskIcon.vue';
+import StockIcon from './Icons/StockIcon.vue';
+import PaymentIcon from './Icons/PaymentIcon.vue';
+import SettingIcon from './Icons/SettingIcon.vue';
+import MemberIcon from './Icons/MemberIcon.vue';
+import DashboardIcon from './Icons/DashboardIcon.vue';
+import AppointmentIcon from './Icons/AppointmentIcon.vue';
+import InventoryIcon from './Icons/InventoryIcon.vue';
+
+const menuPatient = [
+    {title: '1.1. Registration'},
+    {title: '1.2. Records'},
+    {title: '1.3. Scheduling'},
+];
+
+const menuMembers = [
+    {title: '2.1. Dentist Profiles'},
+    {title: '2.2. Schedule'},
+];
+
+const menuAppointment = [
+    {title: '3.1. Booking',},
+    {title: '3.2. Automatic Reminders'},
+    {title: '3.2. Real-time ', },
+];
+
+const menuTreatment = [
+    {title: '4.1. Treatment Plans'},
+    {title: '4.2. '},
+];
+const menuReports = [
+    {title: '5.1. '},
+    {title: '5.2. '},
+    {title: '5.3. '},
+    {title: '5.4. '},
+    {title: '5.5. '},
+    {title: '5.6. '},
+];
+
+const menuSettings = [
+    {title: '6.1. '},
+    {title: '6.2. ', href: "dashboard.index"},
+    {title: '6.3. '},
+];
+
+const prop = defineProps({
+    isCollapsed: Boolean,
+})
+
+</script>
 
 <template>
-    <aside class="w-64 bg-slate-100">
-        <p>This Side bar</p>
+    <aside :class="{ 'sidebar-width': true, 'sidebar-collapsed' : isCollapsed }"
+           class="flex-shrink-0 h-full bg-primaryColor text-white overflow-y-auto">
+        <MenuItem
+            :index="1"
+            title="Dashboard"
+            :href="route('dashboard.index')"
+        >
+            <template v-slot:icon>
+                <DashboardIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="2"
+            title="Patient Management"
+            :items="menuPatient"
+        >
+            <template v-slot:icon>
+                <StockIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="3"
+            title="Dentist Management"
+            :items="menuMembers"
+        >
+            <template v-slot:icon>
+                <MemberIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="4"
+            title="Appointment"
+            :items="menuAppointment"
+        >
+            <template v-slot:icon>
+                <AppointmentIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="5"
+            title="Treatment"
+            :items="menuTreatment"
+        >
+            <template v-slot:icon>
+                <RiskIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="6"
+            title="Payment "
+            :items="menuTreatment"
+        >
+            <template v-slot:icon>
+                <PaymentIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="7"
+            title="Inventory"
+            :items="menuTreatment"
+        >
+            <template v-slot:icon>
+                <InventoryIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="8"
+            title="Reports"
+            :items="menuReports"
+        >
+            <template v-slot:icon>
+                <ReportIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="9"
+            title="Notification"
+            :href="route('dashboard.index')"
+        >
+            <template v-slot:icon>
+                <NotificationIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
+        <MenuItem
+            :index="10"
+            title="Settings"
+            :items="menuSettings"
+        >
+            <template v-slot:icon>
+                <SettingIcon
+                    fill="white"
+                />
+            </template>
+        </MenuItem>
     </aside>
 </template>
 
-<style></style>
+<style scoped>
+</style>
---
+++ resources/js/Components/SubMenuItem.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Link } from '@inertiajs/vue3'
+
+const props = defineProps({
+    subIndex: Number,
+    title: String,
+    href: String,
+})
+</script>
+
+<template>
+    <Link :href="route(href)" :key="subIndex" class="menu-item hover:bg-lightBlueHover rounded-bl-md rounded-tl-md" v-if="href">
+        <p class="flex-auto py-[6px] px-3 text-nowrap">
+            {{title}}
+        </p>
+    </Link>
+    <Link href="#" class="menu-item" v-else>
+        <p class="flex-auto py-[6px] px-3 text-nowrap hover:bg-lightBlueHover rounded-bl-md rounded-tl-md">
+            {{title}}
+        </p>
+    </Link>
+</template>
+
+<style scoped></style>
